7fb87b5853301eebc942c3a7ec8f19e237be9ce1
Alessio <alessiogdanz@gmail.com>
2020-04-09 11:35:22 -0500

    Fixed undefined behavior and code now works with udebug sample inputs

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 int main(){
     string input;
-    int idx = -1;
+    int idx = 0, idx2 = 0;
     while (getline(cin, input)){
-        for (string::iterator i = input.begin(); i != input.end(); ++i){
+        string::iterator init = input.begin(), end = input.end(), i;
+        i = init;
+        while (i != end){
             if (*i == '\"'){
-                idx++;
                 if((idx%2 == 0) || (idx==0)){
                     *i = '`';
                     input.insert(i,'`');
@@ -17,11 +18,17 @@ int main(){
                     *i = '\'';
                     input.insert(i,'\'');
                 }
+            idx++;
+            // Since string is updated during loop, string limits have to be updated
+            init = input.begin();
+            end = input.end();
+            i = init+idx2; // Use of idx2 so it does not have to start the loop from the beginning
             }
+        i++;
+        idx2++;
         }
+        idx2 = 0; // idx2 is per input line, so it has to be reseted
         cout << input << endl;
-        
     }
-    
     return EXIT_SUCCESS;
 }
